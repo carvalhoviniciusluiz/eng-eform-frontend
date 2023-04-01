5b63d0ace91b60ed1700da795f8ad0443bfa4944
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2023-04-01 11:32:11 -0300

    fix: add children on root level of the questions

--- src/app/presentation/pages/form/public/store/public-form.tsx
+++ src/app/presentation/pages/form/public/store/public-form.tsx
@@ -13,10 +13,10 @@ import RadioGroup from '@mui/material/RadioGroup';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import {
-  AiFillSave as SaveIcon,
+  AiOutlineRollback as BackIcon,
   AiOutlineDoubleRight as ExpandIcon,
   AiOutlineLogin as LoginIcon,
-  AiOutlineRollback as BackIcon
+  AiFillSave as SaveIcon
 } from 'react-icons/ai';
 import { BiUserCircle as UserIcon } from 'react-icons/bi';
 import { AnswerModel, QuestionModel, QuestionType } from '~/app/domain/models';
@@ -36,7 +36,6 @@ export default function PublicFormComponent({
   const [state, setState] = useState(() => {
     const { form, questions } = data;
     const value = 0;
-
     return {
       form,
       questions,
@@ -45,49 +44,41 @@ export default function PublicFormComponent({
       loading: false
     };
   });
-
   const classes = useStyles();
-
   const router = useRouter();
 
-  const handleSubmit = () => {
+  function handleSubmit() {
     setState(prevState => ({
       ...prevState,
       loading: true
     }));
-
     addPublicForm
       .add(state.selectedQuestions)
       .then(async () => await router.push('/'))
       .catch(error => {
-        console.error(error);
-
         setState(prevState => ({
           ...prevState,
           loading: false
         }));
       });
-  };
+  }
 
-  const handleQuestionTypeMultipleStore = (
+  function handleQuestionTypeMultipleStore(
     event: any,
     question: QuestionModel
-  ) => {
+  ) {
     setState(prevState => {
       const questionsState = prevState.selectedQuestions;
       const questions = questionsState[question.id] || [];
       const { checked, value } = event.target;
-
       if (checked) {
         questions.push(value);
       } else {
         const index = questions.indexOf(value);
-
         if (index > -1) {
           questions.splice(index, 1);
         }
       }
-
       return {
         ...prevState,
         selectedQuestions: {
@@ -96,12 +87,12 @@ export default function PublicFormComponent({
         }
       };
     });
-  };
+  }
 
-  const handleQuestionTypeObjectiveStore = (
+  function handleQuestionTypeObjectiveStore(
     event: any,
     question: QuestionModel
-  ) => {
+  ) {
     setState(prevState => ({
       ...prevState,
       selectedQuestions: {
@@ -109,9 +100,9 @@ export default function PublicFormComponent({
         [question.id]: event.target.value
       }
     }));
-  };
+  }
 
-  const handleInputToggle = (question: QuestionModel, answer: AnswerModel) => {
+  function handleInputToggle(question: QuestionModel, answer: AnswerModel) {
     switch (question.type) {
       case QuestionType.OBJECTIVE:
         return (
@@ -125,7 +116,6 @@ export default function PublicFormComponent({
             onClick={event => handleQuestionTypeObjectiveStore(event, question)}
           />
         );
-
       case QuestionType.MULTIPLE:
         return (
           <Checkbox
@@ -140,8 +130,7 @@ export default function PublicFormComponent({
           />
         );
     }
-  };
-
+  }
   return (
     <Box
       style={{
@@ -193,7 +182,6 @@ export default function PublicFormComponent({
             )}
           </Typography>
         </Box>
-
         {logged ? (
           <UserIcon size={32} fill={'#1D2438'} />
         ) : (
@@ -202,7 +190,6 @@ export default function PublicFormComponent({
           </Link>
         )}
       </header>
-
       <Box
         style={{
           display: 'flex',
@@ -235,7 +222,6 @@ export default function PublicFormComponent({
               {state.form.status}
             </Typography>
           </Box>
-
           {state.questions?.map(question => (
             <Accordion key={question.id}>
               <AccordionSummary
@@ -248,7 +234,24 @@ export default function PublicFormComponent({
                 </Typography>
               </AccordionSummary>
               <AccordionDetails>
-                {question.children ? (
+                {question.answers && (
+                  <RadioGroup
+                    aria-labelledby={`${question.id}-buttons-group-label`}
+                    defaultValue='female'
+                    name={`${question.id}-radio-buttons-group`}
+                    style={{ marginBottom: 22 }}
+                  >
+                    {question.answers.map(answer => (
+                      <FormControlLabel
+                        key={answer.id}
+                        value={answer.id}
+                        control={handleInputToggle(question, answer)}
+                        label={answer.content}
+                      />
+                    ))}
+                  </RadioGroup>
+                )}
+                {question.children && (
                   <>
                     {question.children.map((child, i) => (
                       <Box key={child.id}>
@@ -284,28 +287,12 @@ export default function PublicFormComponent({
                       </Box>
                     ))}
                   </>
-                ) : (
-                  <RadioGroup
-                    aria-labelledby={`${question.id}-buttons-group-label`}
-                    defaultValue='female'
-                    name={`${question.id}-radio-buttons-group`}
-                  >
-                    {question.answers?.map(answer => (
-                      <FormControlLabel
-                        key={answer.id}
-                        value={answer.id}
-                        control={handleInputToggle(question, answer)}
-                        label={answer.content}
-                      />
-                    ))}
-                  </RadioGroup>
                 )}
               </AccordionDetails>
             </Accordion>
           ))}
         </Box>
       </Box>
-
       <Paper
         sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
         elevation={3}
@@ -326,7 +313,6 @@ export default function PublicFormComponent({
             onClick={handleSubmit}
             disabled={state.loading}
           />
-
           <BottomNavigationAction
             label='Voltar'
             icon={
